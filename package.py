cd9e9d6cef3dc9a808bd6e447bea7329372c42d3
Marcelo Sercheli <marcelo.sercheli@hernehill.com>
2024-07-22 11:06:17 -0400

    Add rez files

---
+++ package.py
@@ -0,0 +1,39 @@
+name = "pybind11"
+
+version = "0.1.0"
+
+authors = [
+    "PyBind",
+]
+
+description = (
+    """Lightweight header-only library that exposes C++ types in Python and vice-versa"""
+)
+
+with scope("config") as c:
+    import os
+
+    c.release_packages_path = os.environ["HH_REZ_REPO_RELEASE_EXT"]
+
+requires = []
+
+private_build_requires = []
+
+variants = [
+    "python-3.7",
+    "python-3.9",
+    "python-3.10",
+    "python-3.11",
+    "python-3.12",
+]
+
+
+def commands():
+    env.REZ_PYBIND11_ROOT = "{root}"
+    env.PYBIND11_ROOT_DIR = "{root}"
+    env.PYBIND11_ROOT = "{root}"
+    env.pybind11_ROOT = "{root}"
+    env.pybind11_DIR = "{root}/share/cmake/pybind11"
+
+
+uuid = "repository.pybind11"
